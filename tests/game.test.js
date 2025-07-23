ae6b5279ced81c140306eb636471008006b8b96a
kylewoodruff <kylewoodruff@gmail.com>
2025-07-23 18:28:34 -0500

    Add edge-case tests for findLetterIndices

--- tests/game.test.js
+++ tests/game.test.js
@@ -9,3 +9,15 @@ test('findLetterIndices finds all repeated letters', () => {
   expect(findLetterIndices('Mississippi', 's')).toEqual([2,3,5,6]);
   expect(findLetterIndices('apple', 'p')).toEqual([1,2]);
 });
+
+test('findLetterIndices works with array input', () => {
+  expect(findLetterIndices(['A', 'b'], 'a')).toEqual([0]);
+  expect(findLetterIndices(['A', 'b'], 'b')).toEqual([1]);
+});
+
+test('findLetterIndices handles empty or undefined arguments', () => {
+  expect(findLetterIndices('', 'a')).toEqual([]);
+  expect(findLetterIndices('hello', '')).toEqual([]);
+  expect(findLetterIndices(undefined, 'a')).toEqual([]);
+  expect(findLetterIndices('hello', undefined)).toEqual([]);
+});
